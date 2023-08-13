8a88a0c54fca60b1ac5b557db77f85a2eefec410
Ibrahim Hossain <ibuhossain3@gmail.com>
2023-08-13 23:17:14 +0600

    update: login form

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -16,7 +16,8 @@ const Login = () => {
     setNumber("+880" + number);
   };
 
-  const loginHandler = () => {
+  const loginHandler = (e) => {
+    e.preventDefault();
     const bangladeshiPhoneNumberRegex = /^\+8801[3-9]\d{8}$/;
     const verifyNumber = bangladeshiPhoneNumberRegex.test(number);
     if (verifyNumber && password.length > 0) {
@@ -43,79 +44,81 @@ const Login = () => {
   return (
     <div className="lg:w-9/12 p-5 m-auto">
       <div className="flex justify-center items-center h-[75vh]">
-        <div className="flex flex-col items-start gap-3 bg-white shadow-lg md:px-10 px-5 py-10 rounded-lg">
-          <figure className="w-[200px] m-auto mb-5">
-            <img
-              src="https://chswebapps.co.uk/auth/ZYzTdD0.png"
-              alt="login logo"
-            />
-          </figure>
-          <div className="join">
-            <span
-              className="btn join-item btn-secondary"
-              onClick={countryCodeHandler}
-            >
-              BD
-            </span>
+        <form onSubmit={loginHandler}>
+          <div className="flex flex-col items-start gap-3 bg-white shadow-lg md:px-10 px-5 py-10 rounded-lg">
+            <figure className="w-[200px] m-auto mb-5">
+              <img
+                src="https://chswebapps.co.uk/auth/ZYzTdD0.png"
+                alt="login logo"
+              />
+            </figure>
+            <div className="join">
+              <span
+                className="btn join-item btn-secondary"
+                onClick={countryCodeHandler}
+              >
+                BD
+              </span>
+              <input
+                type="tel"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+                className="join-item input input-bordered input-primary w-full max-w-xs"
+              />
+            </div>
             <input
-              type="tel"
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
-              className="join-item input input-bordered input-primary w-full max-w-xs"
+              type="password"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="input input-bordered input-primary w-full max-w-xs"
             />
+            <p className="text-xs px-2">
+              Don't have an account?{" "}
+              <Link to="/registration" className="link-hover link-info">
+                Register
+              </Link>
+            </p>
+            {error && (
+              <div className="alert alert-error">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>Error! Invalid phone number.</span>
+              </div>
+            )}
+            {success && (
+              <div className="alert alert-success">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>Successfully loged in</span>
+              </div>
+            )}
+            <button className="btn btn-info" onClick={loginHandler}>
+              Login
+            </button>
           </div>
-          <input
-            type="password"
-            placeholder="Enter password"
-            onChange={(e) => setPassword(e.target.value)}
-            className="input input-bordered input-primary w-full max-w-xs"
-          />
-          <p className="text-xs px-2">
-            Don't have an account?{" "}
-            <Link to="/registration" className="link-hover link-info">
-              Register
-            </Link>
-          </p>
-          {error && (
-            <div className="alert alert-error">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="stroke-current shrink-0 h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span>Error! Invalid phone number.</span>
-            </div>
-          )}
-          {success && (
-            <div className="alert alert-success">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="stroke-current shrink-0 h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span>Successfully loged in</span>
-            </div>
-          )}
-          <button className="btn btn-info" onClick={loginHandler}>
-            Login
-          </button>
-        </div>
+        </form>
       </div>
     </div>
   );
